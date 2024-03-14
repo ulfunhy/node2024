7adec3ea88b86c5ab8fc6f2e307e7a4c26978b55
HyunJin <myfamily11141@gmail.com>
2024-03-14 15:55:29 +0900

    함수기본

---
+++ grammar/03 function.js
@@ -0,0 +1,13 @@
+function 문자출력함수(문자) {
+    console.log('문자출력함수 ' + 문자)
+}
+문자출력함수('a');
+
+// 함수명을 변수처럼 사용
+printChar = 문자출력함수;
+printChar('b');
+
+익명함수 = function(문자) {
+    console.log('익명함수 ' + 문자)
+}
+익명함수('c')
