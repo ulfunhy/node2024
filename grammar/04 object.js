d950f9ea9b25f2005a027a6095f4072ad9811753
HyunJin <myfamily11141@gmail.com>
2024-03-18 10:55:01 +0900

    object

---
+++ grammar/04 object.js
@@ -0,0 +1,10 @@
+const cathy = {
+    name : "cathy",
+    // 속성 이름은 따옴표 처리 안 해도 가능함
+    "age" : 19,
+    skills : ["자바스크립트", "파이썬", "코볼"]
+}
+
+console.log(cathy)
+console.log(cathy.name)
+console.log(cathy["name"])
