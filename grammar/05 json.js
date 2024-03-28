346023823623b0d74232b251eee1c188e5b853f5
HyunJin <myfamily11141@gmail.com>
2024-03-28 16:07:53 +0900

    [수정] 파일명 수정

--- grammar/05 json.js
+++ grammar/05 json.js
@@ -12,7 +12,7 @@ fs.readFile(path.join(__dirname, 'external_data.json')
         console.log(data)   // 받은 데이터의 아스키코드 출력\
         
         // JSON 텍스트를 object로 변환
-        const jsonObject = JSON.parse(data)
+        const jsonObject = JSON.parse(data) 
         console.log(jsonObject) 
 
         // object를 JSON형태의 텍스트로
