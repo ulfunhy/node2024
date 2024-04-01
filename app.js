44ed565bdcc9ec454b6f7175b8a5c6365a167bb4
HyunJin <myfamily11141@gmail.com>
2024-04-01 11:33:33 +0900

    익스프레스 static

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express')
+const path = require('path')    // node에서 기본으로 제공하는 패키지
 const app = express()
 const port = 3000
 
+app.use('/html', express.static(path.join(__dirname, 'html')))
+
 // get method 요청에 대한 라우팅
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})  // localhost:3000
-
-app.get('/cute', (req, res) => {
-    res.send('cute')
-})  // localhost:3000/cute - cute 출력
+    res.send('Test')
+})  // localhost:3000 - Test 출력
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
+    console.log(express.static(path.join(__dirname, 'html')))
 })
